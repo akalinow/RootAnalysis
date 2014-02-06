47f73a7e21d8d7ed804816a1b35640b34e037767
akalinow <Artur.Kalinowski@cern.ch>
2014-02-06 17:43:21 +0100

    A

---
+++ include/EventBase.h
@@ -0,0 +1,17 @@
+/*
+ * EventBase.h
+ *
+ *  Created on: 6 lut 2014
+ *      Author: akalinow
+ */
+
+#ifndef EVENTBASE_H_
+#define EVENTBASE_H_
+
+class EventBase {
+public:
+	EventBase();
+	virtual ~EventBase();
+};
+
+#endif /* EVENTBASE_H_ */
---
+++ src/EventBase.cc
@@ -0,0 +1,18 @@
+/*
+ * EventBase.cpp
+ *
+ *  Created on: 6 lut 2014
+ *      Author: akalinow
+ */
+
+#include "EventBase.h"
+
+EventBase::EventBase() {
+	// TODO Auto-generated constructor stub
+
+}
+
+EventBase::~EventBase() {
+	// TODO Auto-generated destructor stub
+}
+
