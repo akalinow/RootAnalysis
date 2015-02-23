6d48530ce8e5bafa0db76bd620d1dc3fb387d235
Artur Kalinowski <Artur.Kalinowski@cern.ch>
2015-02-23 15:54:16 +0100

    a

--- src/EventProxyBase.cc
+++ src/EventProxyBase.cc
@@ -4,8 +4,6 @@
 EventProxyBase::EventProxyBase():
   fileNames_(), treeName_(""), eventIndex_(0), accumulatedSize_(0){
 
-	std::cout<<__func__<<std::endl;
-
 }
 
 void EventProxyBase::init(std::vector<std::string> const& iFileNames){
