32797863b43709793ab5428b667fd79827a4ff8a
Artur Kalinowski <Artur.Kalinowski@cern.ch>
2015-09-29 21:54:37 +0200

    * add HTTAnalysis * clean up

---
+++ HTTAnalysis/EventProxyHTT.h
@@ -0,0 +1,31 @@
+#ifndef EVENTPROXYHTT_h
+#define EVENTPROXYHTT_h
+
+#include <string>
+#include <typeinfo>
+#include <vector>
+#include "boost/shared_ptr.hpp"
+
+#include "EventProxyBase.h"
+//#include "HTTEvent.h"
+
+#include "TBranch.h"
+
+   class EventProxyHTT: public EventProxyBase{
+
+   public:
+
+      EventProxyHTT();
+      virtual ~EventProxyHTT();
+
+      void init(std::vector<std::string> const& iFileNames);
+
+      int npv;
+      
+   private:
+
+      // List of branches
+      TBranch        *b_npv;
+
+   };
+#endif
---
+++ HTTAnalysis/HTTWeightsMaker.h
@@ -0,0 +1,72 @@
+#ifndef RootAnalysis_HTTWeightsMaker_H
+#define RootAnalysis_HTTWeightsMaker_H
+
+#include <string>
+
+#include "ObjectMessenger.h"
+#include "EventProxyBase.h"
+
+#include "strbitset.h"
+#include "TFileDirectory.h"
+
+//ROOT includes
+#include "TTree.h"
+#include "TList.h"
+#include "TRandom3.h"
+
+#include "Analyzer.h"
+
+class EventProxyHTT;
+
+class HTTWeightsMaker: public Analyzer{
+
+ public:
+  
+  HTTWeightsMaker(const std::string & aName);
+
+  virtual ~HTTWeightsMaker();
+  
+  ///Initialize the analyzer
+  virtual void initialize(TFileDirectory& aDir,
+			  pat::strbitset *aSelections);
+
+  virtual bool analyze(const EventProxyBase& iEvent);
+
+  virtual bool analyze(const EventProxyBase& iEvent, ObjectMessenger *aMessenger){return analyze(iEvent); }
+
+  virtual void finalize();
+
+  virtual void clear(){;};
+
+  virtual void addBranch(TTree *tree);
+
+  virtual void addCutHistos(TList *aList){;};
+
+  bool filter() const{ return filterEvent_;};
+
+ protected:
+
+  pat::strbitset *mySelections_;
+
+  ///Types of the selection flow
+  std::vector<std::string> selectionFlavours_;
+
+ private:
+
+  ///Event weight necessary to scale the MC PU distribution
+  ///to one observed in data.
+  float puWeight;
+
+  ///ROOT file with PU histogram
+  TFile *puFile;
+
+  ///The PU histogram
+  TH1F *hPU;
+
+  
+  //should this HTTWeightsMaker be able to filter events
+  bool filterEvent_;
+ 
+};
+
+#endif
