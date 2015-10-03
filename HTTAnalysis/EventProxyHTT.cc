fd203865160561e6d04058b6c5bed5aa25f62a47
Artur Kalinowski <Artur.Kalinowski@cern.ch>
2015-10-03 21:43:16 +0200

    * add commonUtils.h file with common histograms tools * add plotting of the stacked histograms with a ratio plot * MISSING: proper normalisation of files. At the moment only PU reweighting is   working.

--- HTTAnalysis/EventProxyHTT.cc
+++ HTTAnalysis/EventProxyHTT.cc
@@ -27,29 +27,16 @@ void EventProxyHTT::init(std::vector<std::string> const& iFileNames){
 	TChain *fFriendChainWeights = new TChain("Summary/tree");	
 	TFile *file = new TFile("RootAnalysis_Weights.root");
 	TTree *treeWeights;
-	if(file->IsOpen()){
-	  treeWeights = (TTree*)file->Get("Summary/tree");
-	  //fFriendChainWeights->Add("RootAnalysis_Weights.root",-1);
-	}
-	/////////////
-	//fChain->AddFriend(treeWeights,"weights");
+	if(file->IsOpen()) treeWeights = (TTree*)file->Get("Summary/tree");
 	fChain->AddFriend(treeWeights);
-	
-	//fChain->GetFriend("weights")->Print();
-
-	TObjArray *aObjArray = fChain->GetListOfBranches();
-	for(unsigned int i=0;i<aObjArray->GetSize();++i){
-	  TBranch *branch = (TBranch*)aObjArray->At(i);
-	  //if(branch) std::cout<<branch->GetName()<<std::endl;
-	}
-
+	//////////	
 	puWeight = -1;
 	
 	fChain->SetBranchAddress("npv",&npv);
 	fChain->SetBranchAddress("run",&run);	
 	fChain->SetBranchAddress("svfit",&svfit);
 	fChain->SetBranchAddress("PUWeight",&puWeight);
-	
+
 	fChain->SetBranchStatus("*",0);
 	fChain->SetBranchStatus("npv",1);
 	fChain->SetBranchStatus("run",1);
