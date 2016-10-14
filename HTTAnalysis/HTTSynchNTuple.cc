1306ab0468580cfe4684e6c021719bec94fe0c98
apyskir <andrzej.pyskir@student.uw.edu.pl>
2016-10-14 11:22:47 +0200

    *tauID filling fixed

--- HTTAnalysis/HTTSynchNTuple.cc
+++ HTTAnalysis/HTTSynchNTuple.cc
@@ -29,13 +29,14 @@ void HTTSynchNTuple::fillLegsSpecific(const HTTParticle &leg1, const HTTParticle
   //Leg2: tau
   iso_2 = leg2.getProperty(PropertyEnum::byIsolationMVArun2v1DBoldDMwLTraw);
   byCombinedIsolationDeltaBetaCorrRaw3Hits_2 = leg2.getProperty(PropertyEnum::byCombinedIsolationDeltaBetaCorrRaw3Hits);
-  againstElectronLooseMVA6_2 = (leg2.getProperty(PropertyEnum::tauID) & 1<<10) == 1<<10;				
-  againstElectronMediumMVA6_2 = (leg2.getProperty(PropertyEnum::tauID) & 1<<11) == 1<<11;
-  againstElectronTightMVA6_2 = (leg2.getProperty(PropertyEnum::tauID) & 1<<12) == 1<<12;
-  againstElectronVLooseMVA6_2 = (leg2.getProperty(PropertyEnum::tauID) & 1<<9) == 1<<9;
-  againstElectronVTightMVA6_2 = (leg2.getProperty(PropertyEnum::tauID) & 1<<13) == 1<<13;
-  againstMuonLoose3_2 = (leg2.getProperty(PropertyEnum::tauID) & 1<<7) == 1<<7;
-  againstMuonTight3_2 = (leg2.getProperty(PropertyEnum::tauID) & 1<<8) == 1<<8;
+  int TauID = (int)leg2.getProperty(PropertyEnum::tauID);
+  againstElectronLooseMVA6_2 = (TauID & 1<<10) == 1<<10;				
+  againstElectronMediumMVA6_2 = (TauID & 1<<11) == 1<<11;
+  againstElectronTightMVA6_2 = (TauID & 1<<12) == 1<<12;
+  againstElectronVLooseMVA6_2 = (TauID & 1<<9) == 1<<9;
+  againstElectronVTightMVA6_2 = (TauID & 1<<13) == 1<<13;
+  againstMuonLoose3_2 = (TauID & 1<<7) == 1<<7;
+  againstMuonTight3_2 = (TauID & 1<<8) == 1<<8;
   /*
     chargedIsoPtSum_2;
     decayModeFindingOldDMs_2;
--- HTTAnalysis/HTTSynchNTupleTT.cc
+++ HTTAnalysis/HTTSynchNTupleTT.cc
@@ -22,13 +22,14 @@ void HTTSynchNTupleTT::fillLegsSpecific(const HTTParticle &leg1, const HTTPartic
   //Leg1: leading tau
   iso_1 = leg1.getProperty(PropertyEnum::byIsolationMVArun2v1DBoldDMwLTraw);
   byCombinedIsolationDeltaBetaCorrRaw3Hits_1 = leg1.getProperty(PropertyEnum::byCombinedIsolationDeltaBetaCorrRaw3Hits);
-  againstElectronLooseMVA6_1 = (leg2.getProperty(PropertyEnum::tauID) & 1<<10) == 1<<10;				
-  againstElectronMediumMVA6_1 = (leg2.getProperty(PropertyEnum::tauID) & 1<<11) == 1<<11;
-  againstElectronTightMVA6_1 = (leg2.getProperty(PropertyEnum::tauID) & 1<<12) == 1<<12;
-  againstElectronVLooseMVA6_1 = (leg2.getProperty(PropertyEnum::tauID) & 1<<9) == 1<<9;
-  againstElectronVTightMVA6_1 = (leg2.getProperty(PropertyEnum::tauID) & 1<<13) == 1<<13;
-  againstMuonLoose3_1 = (leg2.getProperty(PropertyEnum::tauID) & 1<<7) == 1<<7;
-  againstMuonTight3_1 = (leg2.getProperty(PropertyEnum::tauID) & 1<<8) == 1<<8;
+  int TauID = (int)leg1.getProperty(PropertyEnum::tauID);
+  againstElectronLooseMVA6_1 = (TauID & 1<<10) == 1<<10;				
+  againstElectronMediumMVA6_1 = (TauID & 1<<11) == 1<<11;
+  againstElectronTightMVA6_1 = (TauID & 1<<12) == 1<<12;
+  againstElectronVLooseMVA6_1 = (TauID & 1<<9) == 1<<9;
+  againstElectronVTightMVA6_1 = (TauID & 1<<13) == 1<<13;
+  againstMuonLoose3_1 = (TauID & 1<<7) == 1<<7;
+  againstMuonTight3_1 = (TauID & 1<<8) == 1<<8;
   /*
     chargedIsoPtSum_1;
     decayModeFindingOldDMs_1;
@@ -41,13 +42,14 @@ void HTTSynchNTupleTT::fillLegsSpecific(const HTTParticle &leg1, const HTTPartic
   //Leg2: trailing tau
   iso_2 = leg2.getProperty(PropertyEnum::byIsolationMVArun2v1DBoldDMwLTraw);
   byCombinedIsolationDeltaBetaCorrRaw3Hits_2 = leg2.getProperty(PropertyEnum::byCombinedIsolationDeltaBetaCorrRaw3Hits);
-  againstElectronLooseMVA6_2 = (leg2.getProperty(PropertyEnum::tauID) & 1<<10) == 1<<10;				
-  againstElectronMediumMVA6_2 = (leg2.getProperty(PropertyEnum::tauID) & 1<<11) == 1<<11;
-  againstElectronTightMVA6_2 = (leg2.getProperty(PropertyEnum::tauID) & 1<<12) == 1<<12;
-  againstElectronVLooseMVA6_2 = (leg2.getProperty(PropertyEnum::tauID) & 1<<9) == 1<<9;
-  againstElectronVTightMVA6_2 = (leg2.getProperty(PropertyEnum::tauID) & 1<<13) == 1<<13;
-  againstMuonLoose3_2 = (leg2.getProperty(PropertyEnum::tauID) & 1<<7) == 1<<7;
-  againstMuonTight3_2 = (leg2.getProperty(PropertyEnum::tauID) & 1<<8) == 1<<8;
+  TauID = (int)leg2.getProperty(PropertyEnum::tauID);
+  againstElectronLooseMVA6_2 = (TauID & 1<<10) == 1<<10;				
+  againstElectronMediumMVA6_2 = (TauID & 1<<11) == 1<<11;
+  againstElectronTightMVA6_2 = (TauID & 1<<12) == 1<<12;
+  againstElectronVLooseMVA6_2 = (TauID & 1<<9) == 1<<9;
+  againstElectronVTightMVA6_2 = (TauID & 1<<13) == 1<<13;
+  againstMuonLoose3_2 = (TauID & 1<<7) == 1<<7;
+  againstMuonTight3_2 = (TauID & 1<<8) == 1<<8;
   /*
     chargedIsoPtSum_2;
     decayModeFindingOldDMs_2;
