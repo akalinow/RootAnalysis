6f85517a5ef68dde7ff78e09916988e2a06c5612
Artur Kalinowski <Artur.Kalinowski@cern.ch>
2018-09-12 16:50:44 +0200

    * change the structure of the whole projest * import HTauTau analysis into the new structure * remove other projest (PAC, OMTF) from the structure

--- test/proof_remote.C
+++
@@ -1,9 +0,0 @@
-{
-  //This make the TSelectors in the library available to the remote proof session
-  gSystem->Load("libRootAnalysis");
-
-gSystem->Setenv("PATH",gSystem->Getenv("PATH1"));
-
-//gSystem->Exec("which python");
-//gSystem->Exec("python -V");
-}
