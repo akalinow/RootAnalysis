f90e5ebccdd642bc02fb101e7e4d6631a3943bb8
Jakub Wiechnik <kuba.wiechnik23@gmail.com>
2023-02-13 13:30:13 +0100

    Addition of beta and vertex coordinates

--- GMT/DataFormats/include/GenObj.h
+++ GMT/DataFormats/include/GenObj.h
@@ -3,11 +3,13 @@
 #include <ostream>
 
 #include "TObject.h"
+// #include "DataFormats/Math/interface/LorentzVector.h"
+
 class GenObj : public TObject {
 public:
  GenObj(float pt=0., float eta=0., float phi=0.,float mass=0., 
-	int charge=0, int pdgid=0, int st=0, int mother=0):
-  _pt(pt),_eta(eta),_phi(phi),_mass(mass),_charge(charge),_id(pdgid),_status(st),_mid(mother) {}
+	int charge=0, int pdgid=0, int st=0, double vx=0., double vy=0., double vz=0., double beta=0):
+  _pt(pt),_eta(eta),_phi(phi),_mass(mass),_charge(charge),_id(pdgid),_status(st),_vx(vx),_vy(vy),_vz(vz),_beta(beta){}
   virtual ~GenObj() {}
 public:
   float pt() const { return _pt;}
@@ -16,15 +18,21 @@ public:
   float mass() const { return _mass;}
   int pdgId() const { return _id;}
   int status() const { return _status;}
-  int motherId() const { return _mid;}
+  // int motherId() const { return _mid;}
   int charge() const { return _charge;}
+  double vx() const { return _vx;}
+  double vy() const { return _vy;}
+  double vz() const { return _vz;}
+  double beta() const { return _beta;}
+
 
 private:  
   float _pt,_eta,_phi,_mass; 
-  int _charge,_id,_status,_mid; 
+  int _charge,_id,_status; 
+  double _vx, _vy, _vz, _beta;
 
 public:
-  ClassDef(GenObj,1);
+  ClassDef(GenObj,5);
 };
 
 std::ostream & operator<< (std::ostream &out, const GenObj &o);
--- GMT/DataFormats/src/GenObj.cc
+++ GMT/DataFormats/src/GenObj.cc
@@ -9,7 +9,15 @@ std::ostream & operator<< (std::ostream &out, const GenObj &o)
   out<<" PDG id: "<<o.pdgId();
   out<<" charge: "<<o.charge();
   out<<"pt: "<<o.pt()<<" eta: "<<o.eta()<<" phi: "<<o.phi();
-  out<<" status: "<<o.status()<<" motherId: "<<o.motherId();
+  out<<" status: "<<o.status();
+  out<<" x coordinate of vertex position: "<<o.vx();
+  out<<" y coordinate of vertex position: "<<o.vy();
+  out<<" z coordinate of vertex position: "<<o.vz();
+  out<<" beta: "<<o.beta();
+
+
+
+
   out<<std::endl;
 
   return out;
